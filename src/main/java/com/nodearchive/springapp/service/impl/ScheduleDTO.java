55680463fca130390944a3ee4128ded08b31f32d
kimhyemin <hyeminkim45@gmail.com>
2023-01-08 16:28:57 +0900

    ScheduleController.java, ScheduleDTO.java (버전 뒤로 돌린 뒤에 main을 다시 CLONE하고 수정사항 넣음)

---
+++ src/main/java/com/nodearchive/springapp/service/impl/ScheduleDTO.java
@@ -0,0 +1,33 @@
+package com.nodearchive.springapp.service.impl;
+
+import java.util.List;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ScheduleDTO {
+	//
+	private int sche_no;//pk - seq 사용
+	private String sche_title;
+	private String sche_content;
+	private java.sql.Timestamp sche_startdate;
+	private java.sql.Timestamp sche_enddate;
+	private int sche_mark;
+	private int sche_status;
+	private String sche_color;
+	
+	
+	//ScheduleRef table 컬럼들
+	private String m_id;
+	private java.sql.Timestamp sr_regidate;
+	
+	
+}
---
+++ src/main/java/com/nodearchive/springapp/web/ScheduleController.java
@@ -0,0 +1,34 @@
+package com.nodearchive.springapp.web;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+//@Controller
+@RequestMapping("/Schedule")
+public class ScheduleController {
+	
+	//주간 달력
+	@RequestMapping("/week.kosmo")
+	public String weekly(
+//			Authentication auth,
+			) {
+		return "schedule/Week";
+	}
+	
+	//월간 달력(메인)
+	@RequestMapping("/month.kosmo")
+	public String monthly(
+			//Authentication auth,
+			) {
+		return "schedule/Month";
+	}
+	
+	//연간 달력
+	@RequestMapping("/year.kosmo")
+	public String yearly(
+			//Authentication auth,
+			) {
+		return "schedule/Year";
+	}
+}
