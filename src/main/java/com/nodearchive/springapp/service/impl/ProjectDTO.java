f09632a9b8ad4201596f39ab5b1ee445cf9acbb3
myosung <rabitmyo@naver.com>
2023-01-09 15:55:17 +0900

    업무/리포팅 관련 DTO 업데이트(Task DTO, Report DTO 삭제)

--- src/main/java/com/nodearchive/springapp/service/impl/ProjectDTO.java
+++ src/main/java/com/nodearchive/springapp/service/impl/ProjectDTO.java
@@ -1,15 +1,41 @@
 package com.nodearchive.springapp.service.impl;
 
+import java.util.Date;
+
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Builder
 @Getter
 @Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class ProjectDTO {
-	String project_no;
-	String project_name;
-	String m_id;
-	int sche_no;
+	//project
+	private int project_no;
+	private String project_name;
+	
+	//task
+	private int task_no;
+	private String task_name;
+	private String task_content;
+	private String task_categoty;
+	
+	//report
+	private String report_no;
+	private java.sql.Date report_regidate;
+	private java.sql.Date report_startdate;
+	private java.sql.Date report_enddate;
+	
+	//schedule 테이블에서 가져올 데이터 
+	private int sche_no;
+	private java.sql.Date sche_startdate;
+	private java.sql.Date sche_enddate;
+	
+	//members 테이블에서 가져올 데이터
+	private String m_id;
+	private String m_name;
 }
