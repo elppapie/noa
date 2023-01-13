1278feb60223b227a0df4878de680f596a1bd2dd
gookyeongbin <md5519@naver.com>
2023-01-13 12:20:20 +0900

    mail.xml/MailDAO.java(메일관련 로직들 생성 및 쿼리 일부 추가-백업용)

--- src/main/java/com/nodearchive/springapp/service/impl/MailDAO.java
+++ src/main/java/com/nodearchive/springapp/service/impl/MailDAO.java
@@ -1,5 +1,8 @@
 package com.nodearchive.springapp.service.impl;
 
+import java.util.List;
+import java.util.Map;
+
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
@@ -11,6 +14,43 @@ public class MailDAO {
 	@Autowired
 	private SqlSessionTemplate template;
 	
+	
+	//받은 메일함 가져오기
+	public List<Map> findRecByMailReceive(Map map){
+		return template.selectList("mailFindByReceive", map);
+	}
+	
+	//보낸 메일함 가져오기
+	public List<Map> findRecByMailSend(Map map){
+		return template.selectList("mailFindBySend", map);
+	}
+	
+	//임시 메일함 가져오기
+	public List<Map> findRecByMailTemp(Map map){
+		return template.selectList("mailFindByTemp", map);		
+	}
+	
+	//내게 쓴 메일함 가져오기
+	public List<Map> findRecByMailMy(Map map){
+		return template.selectList("mailFindByMy", map);
+	}
+	
+	//스팸 메일함 가져오기
+	public List<Map> findRecByMailSpam(Map map){
+		return template.selectList("mailFindBySpan", map);
+	}
+	
+	//즐겨찾기 메일함 가져오기
+	public List<Map> findRecByMailfav(Map map){
+		return template.selectList("mailFindByFav", map);
+	}
+	
+	//휴지통 메일함 가져오기
+	public List<Map> findRecByMailTrash(Map map){
+		return template.selectList("mailFindByTrash", map);
+	}
+	
+	//
 	
 	
 }
